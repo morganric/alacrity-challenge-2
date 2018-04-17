efe66a92755d299ce16dcc551bcba177089c0b00
Richard Morgan <richardmorgan@Richards-MacBook-Pro.local>
2018-04-17 11:21:22 +0100

    first

---
+++ js/users.js
@@ -0,0 +1,14 @@
+function User(name, age) {
+
+	this.name = name;
+	this.age = age;
+
+};
+
+let alice = new User("Alice", 20);
+let bob = new User("Bob", 25);
+let carol = new User("Carol", 30);
+let dave = new User("Dave", 35);
+
+let users = [alice,bob, carol, dave];
+
